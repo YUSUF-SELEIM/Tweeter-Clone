a704b1c397db2530a691fface7598a5f402cdb6d
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-09-11 22:09:28 +0300

    Refactor Home page to update UI, add tweet functionality, and retrieve tweets
    removed auth stuff from here to the authContext

--- app/(authenticated)/home/page.tsx
+++ app/(authenticated)/home/page.tsx
@@ -1,61 +1,33 @@
-"use client";
+// pages/home.tsx
+'use client'; // This ensures the Home page runs on the client
 
-import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
 import TweetForm from '@/components/TweetForm';
 import TweetCard from '@/components/TweetCard';
-import { jwtDecode } from 'jwt-decode';
+import { useAuth } from '@/context/AuthContext';
+import { useEffect, useState } from 'react';
 import { Tweet } from '@/types';
 
 export default function Home() {
-  const router = useRouter();
-  const [loading, setLoading] = useState(true);
+  const { authorId, loading } = useAuth();
   const [tweets, setTweets] = useState<Tweet[]>([]);
-  const [authorId, setAuthorId] = useState<string | null>(null); 
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
-
-    if (!token) {
-      router.push('/');
-      return;
-    }
-
-    const validateToken = async () => {
+    const fetchTweets = async () => {
       try {
-        const response = await fetch('/api/auth/validate', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
-          },
-        });
-
-        if (!response.ok) {
-          router.push('/');
+        const response = await fetch('/api/tweet');
+        if (response.ok) {
+          const tweetsData = await response.json();
+          setTweets(tweetsData);
         } else {
-          const decodedToken: { userId: string } = jwtDecode(token);
-          setAuthorId(decodedToken.userId); // Set the userId from the token
-
-          // Fetch tweets from the API
-          const tweetsResponse = await fetch('/api/tweet');
-          if (tweetsResponse.ok) {
-            const tweetsData = await tweetsResponse.json();
-            setTweets(tweetsData);
-          } else {
-            console.error('Failed to fetch tweets');
-          }
+          console.error('Failed to fetch tweets');
         }
-      } catch (err) {
-        console.error('Error validating token:', err);
-        router.push('/');
-      } finally {
-        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching tweets:', error);
       }
     };
 
-    validateToken();
-  }, [router]);
+    fetchTweets();
+  }, []);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -65,9 +37,9 @@ export default function Home() {
     <div className="flex w-full flex-col items-center md:px-80 px-2 py-4">
       {authorId && <TweetForm authorId={authorId} />}
 
-      <div className="w-full mt-6 space-y-4">
+      <div className="w-full mt-6 space-y-8">
         {tweets.map((tweet) => (
-        <TweetCard key={tweet.id} tweet={tweet} />
+          <TweetCard key={tweet.id} tweet={tweet} authorId={authorId ?? ''}/>
         ))}
       </div>
     </div>
