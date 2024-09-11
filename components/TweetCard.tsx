de081158e4e1f22477ec30155dbbbb5e867f8fa9
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-09-11 22:08:06 +0300

    Refactor TweetCard component to update UI, add comment functionality, and retrieve tweet comments

--- components/TweetCard.tsx
+++ components/TweetCard.tsx
@@ -3,38 +3,45 @@ import { Tweet } from '@/types';
 import CommentsSection from './CommentsSection';
 import TweetActions from './TweetActions';
 import TweetData from './TweetData';
+import { useState, forwardRef } from 'react';
 
-export default function TweetCard({ tweet }: { tweet: Tweet }) {
-  const formattedDate = format(new Date(tweet.createdAt), "d MMMM 'at' HH:mm");
+const TweetCard = forwardRef<HTMLDivElement, { tweet: Tweet, authorId: string }>(
+  ({ tweet, authorId }, ref) => {
+    const [showComments, setShowComments] = useState(false);
+    const formattedDate = format(new Date(tweet.createdAt), "d MMMM 'at' HH:mm");
 
-  return (
-    <div className="w-full border rounded-lg p-4 bg-background">
-      <div className="flex items-center mb-2">
-        <img
-          src={tweet.author.imageUrl || `https://avatar.iran.liara.run/username?username=${tweet.author.username}`}
-          alt={tweet.author.username}
-          className="w-12 h-12  mr-2"
-        />
-        <div className='flex flex-col'>
-          <span className="font-medium">{tweet.author.username}</span>
-          <span className="text-gray-500 text-xs">{formattedDate}</span>
+    return (
+      <div ref={ref} className="w-full border rounded-lg p-4 bg-background">
+        <div className="flex items-center mb-2">
+          <img
+            src={tweet.author.imageUrl || `https://avatar.iran.liara.run/username?username=${tweet.author.username}`}
+            alt={tweet.author.username}
+            className="w-12 h-12  mr-2"
+          />
+          <div className='flex flex-col'>
+            <span className="font-medium">{tweet.author.username}</span>
+            <span className="text-gray-500 text-xs">{formattedDate}</span>
+          </div>
         </div>
-      </div>
-      <p className="my-4 font-light">{tweet.content}</p>
-      {tweet.imageUrl && (
-        <img
-          src={tweet.imageUrl || 'https://via.placeholder.com/40'}
-          alt="Tweet media"
-          className="w-full h-auto rounded-lg"
+        <p className="my-4 font-light">{tweet.content}</p>
+        {tweet.imageUrl && (
+          <img
+            src={tweet.imageUrl || 'https://via.placeholder.com/40'}
+            alt="Tweet media"
+            className="w-full h-auto rounded-lg"
+          />
+        )}
+        <TweetActions setShowComments={setShowComments} />
+        <TweetData
+          comments={tweet.comments}
+          retweets={tweet.retweets}
+          likes={tweet.likes}
         />
-      )}
-      <TweetActions />
-      <TweetData
-        comments={tweet.comments}
-        retweets={tweet.retweets}
-        likes={tweet.likes}
-      />
-      <CommentsSection comments={tweet.commentsList} />
-    </div>
-  );
-}
+        <CommentsSection tweetId={tweet.id} authorId={authorId} showComments={showComments} />
+      </div>
+    );
+  }
+);
+
+TweetCard.displayName = 'TweetCard';
+export default TweetCard;
