3cee47d5fad2ee3f15dbe3d2ffcc4e952823fe59
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-09-11 20:18:10 +0300

    Refactor TweetCard component to display author's profile image, username, and tweet creation date

--- components/TweetCard.tsx
+++ components/TweetCard.tsx
@@ -1,22 +1,26 @@
+import { format } from 'date-fns';
 import { Tweet } from '@/types';
 import CommentsSection from './CommentsSection';
 import TweetActions from './TweetActions';
+import TweetData from './TweetData';
 
 export default function TweetCard({ tweet }: { tweet: Tweet }) {
+  const formattedDate = format(new Date(tweet.createdAt), "d MMMM 'at' HH:mm");
+
   return (
     <div className="w-full border rounded-lg p-4 bg-background">
       <div className="flex items-center mb-2">
-        {tweet.imageUrl && (
-          <img
-            src={tweet.author.imageUrl || 'https://via.placeholder.com/40'}
-            alt={tweet.author.username}
-            className="w-10 h-10 rounded-full mr-2"
-          />
-        )}
-        <span className="font-semibold">{tweet.author.username}</span>
+        <img
+          src={tweet.author.imageUrl || `https://avatar.iran.liara.run/username?username=${tweet.author.username}`}
+          alt={tweet.author.username}
+          className="w-12 h-12  mr-2"
+        />
+        <div className='flex flex-col'>
+          <span className="font-medium">{tweet.author.username}</span>
+          <span className="text-gray-500 text-xs">{formattedDate}</span>
+        </div>
       </div>
-      
-      <p className="mb-2">{tweet.content}</p>
+      <p className="my-4 font-light">{tweet.content}</p>
       {tweet.imageUrl && (
         <img
           src={tweet.imageUrl || 'https://via.placeholder.com/40'}
@@ -24,13 +28,12 @@ export default function TweetCard({ tweet }: { tweet: Tweet }) {
           className="w-full h-auto rounded-lg"
         />
       )}
-
-      <TweetActions 
-        comments={tweet.comments} 
-        retweets={tweet.retweets} 
-        likes={tweet.likes} 
+      <TweetActions />
+      <TweetData
+        comments={tweet.comments}
+        retweets={tweet.retweets}
+        likes={tweet.likes}
       />
-      
       <CommentsSection comments={tweet.commentsList} />
     </div>
   );
