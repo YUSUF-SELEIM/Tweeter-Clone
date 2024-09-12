adf475439d0f5dc61f6347953aac519a7aa34350
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-09-12 13:28:25 +0300

    Refactor TweetCard component to include profile link functionality

--- components/TweetCard.tsx
+++ components/TweetCard.tsx
@@ -4,19 +4,25 @@ import CommentsSection from './CommentsSection';
 import TweetActions from './TweetActions';
 import TweetData from './TweetData';
 import { useState, forwardRef } from 'react';
+import { useRouter } from 'next/navigation'; 
 
 const TweetCard = forwardRef<HTMLDivElement, { tweet: Tweet, authorId: string }>(
   ({ tweet, authorId }, ref) => {
     const [showComments, setShowComments] = useState(false);
     const formattedDate = format(new Date(tweet.createdAt), "d MMMM 'at' HH:mm");
+    const router = useRouter(); 
+
+    const handleProfileClick = () => {
+      router.push(`/profile/${tweet.author.id}`);
+    };
 
     return (
       <div ref={ref} className="w-full border rounded-lg p-4 bg-background">
-        <div className="flex items-center mb-2">
+        <div className="flex items-center mb-2 cursor-pointer" onClick={handleProfileClick}>
           <img
             src={tweet.author.imageUrl || `https://avatar.iran.liara.run/username?username=${tweet.author.username}`}
             alt={tweet.author.username}
-            className="w-12 h-12  mr-2"
+            className="w-12 h-12 mr-2"
           />
           <div className='flex flex-col'>
             <span className="font-medium">{tweet.author.username}</span>
