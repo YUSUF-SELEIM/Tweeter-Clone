441bc8c02328027597f4474e6762fea3b4178a69
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-09-12 16:37:21 +0300

    Refactor ProfileHeader component to improve layout and add conditional rendering for follow button

--- components/ProfileHeader.tsx
+++ components/ProfileHeader.tsx
@@ -57,14 +57,14 @@ export default function ProfileHeader({
   };
 
   return (
-    <div className="flex flex-col items-center bg-[#F2F2F2] h-screen">
+    <div className="flex flex-col items-center ">
       {bannerUrl ? (
         <div className="w-full h-48 sm:h-64 bg-cover bg-center" style={{ backgroundImage: `url(${bannerUrl})` }} />
       ) : (
         <div className="w-full h-48 sm:h-64 bg-cover bg-center" style={{ backgroundImage: `url(https://random.imagecdn.app/2000/300)` }} />
       )}
 
-      <div className="relative flex flex-col md:flex-row justify-center md:justify-between items-center bg-white w-[80%] -mt-12 shadow-lg pb-5 rounded-lg p-5">
+      <div className="relative flex flex-col md:flex-row justify-center md:justify-between items-center bg-white w-[80%] -mt-12 shadow-lg pb-12 rounded-lg p-5">
         <div className="relative -mt-16">
           <img
             src={imageUrl || `https://avatar.iran.liara.run/username?username=${username}`}
@@ -77,23 +77,28 @@ export default function ProfileHeader({
           <h1 className="text-2xl font-semibold">{username}</h1>
           {bio && <p className="text-gray-600">{bio}</p>}
         </div>
+
         <ProfileHeaderData
-        followersCount={followersCount}
-        followingCount={followingCount}
-        loading={loading} 
-      />
-        <div className="mt-4 md:-mt-4 md:ml-auto">
-          {loading ? (
-            <div className="w-24 h-8 bg-gray-300 animate-pulse rounded-lg" />
-          ) : (
-            <Button
-              onClick={handleFollowClick}
-              className={`px-4 py-2 rounded-lg ${isFollowing ? 'bg-red-500' : 'bg-blue-500'} text-white`}
-            >
-              {isFollowing ? 'Unfollow' : 'Follow'}
-            </Button>
-          )}
-        </div>
+          followersCount={followersCount}
+          followingCount={followingCount}
+          loading={loading} 
+        />
+
+        {/* Conditionally render the follow button based on the currentUserId and profileId */}
+        {currentUserId !== profileId && (
+          <div className="mt-4 md:-mt-4 md:ml-auto">
+            {loading ? (
+              <div className="w-24 h-8 bg-gray-300 animate-pulse rounded-lg" />
+            ) : (
+              <Button
+                onClick={handleFollowClick}
+                className={`px-4 py-2 rounded-lg ${isFollowing ? 'bg-red-500' : 'bg-blue-500'} text-white`}
+              >
+                {isFollowing ? 'Unfollow' : 'Follow'}
+              </Button>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
