ba1fc008f9d224ab0bf3e796b387d26cd7fffa19
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-09-13 16:57:49 +0300

    Add TrendsPanel and WhoToFollow components to home page

--- app/(authenticated)/home/page.tsx
+++ app/(authenticated)/home/page.tsx
@@ -1,11 +1,12 @@
 'use client';
-
 import TweetForm from '@/components/TweetForm';
 import TweetCard from '@/components/TweetCard';
 import { useAuth } from '@/context/AuthContext';
 import { useEffect, useState } from 'react';
 import { Tweet } from '@/types';
 import { LoadingSpinner } from '@/components/ui/spinner';
+import Trends from '@/components/TrendsPanel';
+import WhoToFollow from '@/components/WhoToFollow';
 
 export default function Home() {
   const { authorId, loading } = useAuth();
@@ -36,14 +37,18 @@ export default function Home() {
   }
 
   return (
-    <div className="flex w-full flex-col items-center md:px-80 px-2 py-4 bg-[##F2F2F2]">
-      {authorId && <TweetForm authorId={authorId} />}
-
-      <div className="w-full mt-6 space-y-8">
+    <div className="flex justify-between w-full px-2 py-4 md:px-24 bg-[##F2F2F2]">
+      <div className="w-full space-y-8">
+        {authorId && <TweetForm authorId={authorId} />}
         {tweets.map((tweet) => (
           <TweetCard key={tweet.id} tweet={tweet} authorId={authorId ?? ''} />
         ))}
       </div>
+
+   <div className="h-full sticky hidden md:block w-2/5 ml-8">
+        <Trends />
+        <WhoToFollow />
+    </div>
     </div>
   );
 }
