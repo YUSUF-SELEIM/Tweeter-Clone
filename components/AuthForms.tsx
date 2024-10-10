3c019ffd0dfb02152d915ded7e85ecf8fdb8b0b9
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-10-10 17:43:57 +0300

    Refactor AuthForms component to add logo

--- components/AuthForms.tsx
+++ components/AuthForms.tsx
@@ -1,19 +1,27 @@
-'use client';
+"use client";
 import { useState } from "react";
 import LoginForm from "@/components/LoginForm";
 import SignUpForm from "@/components/SignUpForm";
+import Image from "next/image";
 
 export default function AuthForms() {
-    const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(true);
 
-    const toggleForm = () => {
-      setIsLogin(!isLogin);
-    };
-  
-    return (
-      <div className="w-full flex flex-col items-center justify-center">
-          <h1 className="text-3xl font-bold text-center text-blue-500 mb-6">Tweeter</h1>
-          {isLogin ? <LoginForm toggleForm={toggleForm}/> : <SignUpForm toggleForm={toggleForm}/>}
+  const toggleForm = () => {
+    setIsLogin(!isLogin);
+  };
+
+  return (
+    <div className="w-full flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center text-3xl font-bold text-center text-blue-600 mb-6 ">
+        <Image src={"/images/logo.png"} alt={"logo"} height={100} width={100} className="hover:rotate-12"/>
+        <div className="tracking-widest">Tweeter</div>
       </div>
-    );
-  }
+      {isLogin ? (
+        <LoginForm toggleForm={toggleForm} />
+      ) : (
+        <SignUpForm toggleForm={toggleForm} />
+      )}
+    </div>
+  );
+}
